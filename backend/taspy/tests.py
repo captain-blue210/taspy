de6b0632f99704452f6806892473e9efac6a6f38
Aoki Hayato <67002842+captain-blue210@users.noreply.github.com>
2020-08-30 21:04:44 +0900

    test:add tests (#8)

--- backend/taspy/tests.py
+++ backend/taspy/tests.py
@@ -18,7 +18,7 @@ class TaskTests(TestCase):
             status = 'OPEN'
         )
 
-        res = self.client.get(reverse('task-detail', args=(1,)))
+        res = self.client.get(reverse('task-detail', args=(task.id,)))
         self.assertContains(res, 'テスト')
         self.assertContains(res, '2020-08-27')
         self.assertContains(res, 'OPEN')
@@ -50,3 +50,64 @@ class TaskTests(TestCase):
         self.assertEqual(data_list[1]['name'], task2.name)
         self.assertEqual(data_list[2]['name'], task3.name)
 
+    def test_create_task(self):
+        """
+        タスクを１つ作成できる
+        """
+        data = {
+            'name': '作成テスト',
+            'expirationDm': '2020-08-30',
+            'status': 'OPEN'
+        }
+        res = self.client.post(
+            reverse('task-list'),
+            json.dumps(data),
+            content_type='application/json'
+        )
+
+        actual = Task.objects.get(name='作成テスト')
+        self.assertEqual(Task.objects.count(), 1)
+        self.assertEqual(actual.name, '作成テスト')
+
+    def test_update_task(self):
+        """
+        タスクを更新できる
+        """
+        task = Task.objects.create(
+            name = '更新テスト',
+            expiration_dm = '2020-08-30',
+            status = 'OPEN'
+        )
+
+        res = self.client.put(
+            reverse('task-detail', args=(task.id,)),
+            json.dumps({
+                'id': task.id,
+                'name': '更新済み',
+                'expirationDm': task.expiration_dm,
+                'status': 'CLOSED',
+            }),
+            content_type='application/json'
+        )
+
+        task.refresh_from_db()
+        self.assertEqual(Task.objects.count(), 1)
+        self.assertEqual(Task.objects.get().name, '更新済み')
+        self.assertEqual(Task.objects.get().status, 'CLOSED')
+
+    def test_delete_task(self):
+        """
+        タスクを削除できる
+        """
+        task = Task.objects.create(
+            name = '削除テスト',
+            expiration_dm = '2020-08-30',
+            status = 'OPEN'
+        )
+
+        res = self.client.delete(
+            reverse("task-detail", args=(task.id,)),
+            json.dumps({ 'id': task.id }),
+            content_type='application/json'
+        )
+        self.assertEqual(Task.objects.count(), 0)
